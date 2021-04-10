24f468c40f610755b73febb35f8a8eba717a5242
Isai Vargas <isaidvargas@hotmail.com>
2021-04-09 21:55:48 -0400

    first commit

---
+++ Catalogo/Home.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Catalogo
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            WSDB.BDSoapClient WS = new WSDB.BDSoapClient();
+
+            DataSet ds = WS.GetData();
+
+            GridView1.DataSource = ds.Tables[0];
+
+            GridView1.DataBind();
+        }
+    }
+}
---
+++ WSDB/BD.asmx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data.SqlClient;
+
+namespace WSDB
+{
+    /// <summary>
+    /// Descripción breve de BD
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class BD : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+
+        [WebMethod]
+        public DataSet GetData()
+        {
+            SqlConnection conn = new SqlConnection();
+
+            conn.ConnectionString = "data source=AYYDEE/SQLEXPRESS;initial catalog=Notas;integrated security=True;";
+
+            SqlDataAdapter da = new SqlDataAdapter("select * from EstudianteSet", conn);
+
+            DataSet ds = new DataSet();
+
+            da.Fill(ds);
+
+            return ds;
+        }
+    }
+}
